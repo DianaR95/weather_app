c4a0e0d9d688b517bcc5eb18a0ceaa45b3bc4381
Diana Radulescu <radulescu.diana95@gmail.com>
2024-04-07 19:24:07 +0300

    update curr weather function

--- main.py
+++ main.py
@@ -18,5 +18,5 @@ if __name__ == '__main__':
     config = initialise_config("config.json")
     key = os.environ['weather_api_key']
     weather_dict = weather.get_current_weather(url=config['url'], key=key, city="Cluj")
-    print(weather_dict)
+    print(json.dumps(weather_dict, indent=4))
 
--- weather.py
+++ weather.py
@@ -3,18 +3,17 @@ import json
 import requests
 
 
-def get_current_weather(url: str, key: str, city: str ="Bucharest"):
+def get_current_weather(url:str, key: str, city: str = "Bucharest"):
     try:
-        response = requests.get(url + city, headers={"key": "key"})
+        response = requests.get(url + city, headers={"key": key})
         if response.status_code == 200:
             weather_dict = json.loads(response.text)
-            if weather_dict('error'):
-                raise Exception("Orsaul nu exista in baza de date")
+            if weather_dict.get('error'):
+                raise Exception("Orasul nu exista in baza noastra de date")
             return weather_dict
-
         else:
             raise Exception(f"Something wrong with the api \n"
-                            f"Code: {response.status_code} \n"
+                            f"Code: {response.status_code}\n"
                             f"Message: {response.text}")
     except Exception as e:
         print(e)
